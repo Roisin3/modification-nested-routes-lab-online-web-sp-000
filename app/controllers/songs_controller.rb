b64a543ab7f902ee025a96b5f802f2e69ea012ab
Michael Lynch <michael.lynch8211@gmail.com>
2020-10-14 18:08:14 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,12 @@ class SongsController < ApplicationController
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
         redirect_to artists_path, alert: "Artist not found"
-    @song = Song.find(params[:id])
+      else
+        @song = artist.songs.find_by(id: params[:id])
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
